0b967afa70a76da7346ae1ad939f5a5062a2741b
Will Rowe <w.p.m.rowe@gmail.com>
2019-12-09 14:33:41 +0000

    adding libfswatch as external lib

--- src/watcher.h
+++ src/watcher.h
@@ -1,14 +1,15 @@
 #ifndef WATCHER_H
 #define WATCHER_H
 
-#include <libfswatch/c/libfswatch.h>
+#include "external/libfswatch/c/libfswatch.h"
 #include "bloom.h"
 #include "workerpool.h"
 
 // watcherArgs_t
-typedef struct watcherArgs {
-    tpool_t* workerPool;
-    struct bloom* bloomFilter;
+typedef struct watcherArgs
+{
+    tpool_t *workerPool;
+    struct bloom *bloomFilter;
     char filepath[50];
     int k_size;
     int sketch_size;
@@ -18,7 +19,7 @@ typedef struct watcherArgs {
 /*
     function prototypes
 */
-char* getExt(const char *filename);
-void watcherCallback(fsw_cevent const * const events, const unsigned int event_num, void* args);
+char *getExt(const char *filename);
+void watcherCallback(fsw_cevent const *const events, const unsigned int event_num, void *args);
 
 #endif
