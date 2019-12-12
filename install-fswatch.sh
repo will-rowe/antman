9a1dc7f494f4e5f20606046c01eda5df1359d91f
Will Rowe <w.p.m.rowe@gmail.com>
2019-12-12 21:37:37 +0000

    downgrading fswatch in the install helper

--- install-fswatch.sh
+++ install-fswatch.sh
@@ -3,9 +3,9 @@
 # a helper script to download, configure and compile an fswatch release
  
 mkdir fswatch && cd $_
-wget -O fswatch.tar.gz https://github.com/emcrisostomo/fswatch/releases/download/1.14.0/fswatch-1.14.0.tar.gz
+wget -O fswatch.tar.gz https://github.com/emcrisostomo/fswatch/releases/download/1.13.0/fswatch-1.13.0.tar.gz
 tar -xvf fswatch.tar.gz -C ./ --strip-components 1
-./configure
+./configure CFLAGS="-g -Wall -O2 -Wc++-compat -std=c99"
 make
 make install
 cd ..
